88122aeb66c36b10ed0f1326798aa0b0ffeaf575
ahmed <ahmedtm2000@gmail.com>
2019-04-28 02:36:59 +0100

    basic circle movement

--- index.js
+++ index.js
@@ -1,23 +1,3 @@
-class Input {
-
-  keys = {}
-
-  constructor() {
-    this.keys = {}
-    document.onkeydown = this.keydown
-    document.onkeyup = this.keyup
-  }
-
-  keydown(key) {
-
-    this.keys[key.key] = true
-  }
-  keyup(key) {
-
-    this.keys[key.key] = false
-  }
-}
-
 class Player {
 
   constructor(rds, cds){
@@ -28,7 +8,7 @@ class Player {
 
   draw(ctx){
     ctx.beginPath();
-    ctx.arc(100, 75, 50, 0, 2 * Math.PI);
+    ctx.arc(this.cds[0], this.cds[1], this.rds, 0, 2 * Math.PI);
     ctx.fill();
   }
 
@@ -38,14 +18,23 @@ class Player {
   }
 }
 
+keys = {}
+document.onkeydown = (key) => {
+
+  keys[key.key] = true
+}
+
+document.onkeyup = (key) => {
+
+  keys[key.key] = false
+}
+
 
 let cvs = document.getElementById('canvas')
 let ctx = cvs.getContext('2d')
 cvs.width = 800
 cvs.height = 600
 cvs.style.border = 'solid black 1px'
-
-input = new Input()
 
 var ws = new WebSocket("wss://138.251.241.114/5000");
 ws.onopen = (e) => {
@@ -55,4 +44,32 @@ ws.onmessage = (e) => {
 }
 ws.onerror = (e) => {
   console.log(e);
+}
+
+player1 = new Player(20, [cvs.width / 2, cvs.height / 2])
+
+setInterval(game, 23)
+
+
+function game(){
+
+  if(keys['a']){
+
+    player1.update([-5,0])
+  }
+  if(keys['s']){
+
+    player1.update([0,5])
+  }
+  if(keys['d']){
+
+    player1.update([5,0])
+  }
+  if(keys['w']){
+
+    player1.update([0,-5])
+  }
+
+  ctx.clearRect(0, 0, cvs.width, cvs.height);
+  player1.draw(ctx)
 }
