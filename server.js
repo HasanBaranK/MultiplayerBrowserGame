613524de1d939062f33b571a1586112498c01872
HasanBaranK <hasanbaran98@gmail.com>
2019-04-28 23:53:40 +0100

    map and collision

--- server.js
+++ server.js
@@ -3,12 +3,6 @@ let path = require('path');
 let socketIO = require('socket.io');
 let express = require('express');
 
-
-
-
-
-
-
 const app = express();
 
 let server = require('http').createServer(app);
@@ -25,6 +19,9 @@ server.listen(5000, function() {
 });
 
 var players = {};
+var collisonMap = {};
+let map = autoMapgenerator(0,1000,10);
+
 io.on('connection', function(socket) {
   console.log('Player ' + socket.id + ' has joined the game');
     socket.on('new player', function() {
@@ -37,15 +34,32 @@ io.on('connection', function(socket) {
         var player = players[socket.id] || {};
         if (data.a) {
             player.x -= 5;
+            if(checkCollision(player,64,64,10)){
+                player.x += 5;
+                console.log("Collision")
+            }
+
         }
         if (data.w) {
             player.y -= 5;
+            if(checkCollision(player,64,64,10)){
+                player.y += 5;
+                console.log("Collision")
+            }
         }
         if (data.d) {
             player.x += 5;
+            if(checkCollision(player,64,64,10)){
+                player.x -= 5;
+                console.log("Collision")
+            }
         }
         if (data.s) {
             player.y += 5;
+            if(checkCollision(player,64,64,10)){
+                player.y -= 5;
+                console.log("Collision")
+            }
         }
     });
     socket.on('disconnect', function(some) {
@@ -53,6 +67,64 @@ io.on('connection', function(socket) {
       players[socket.id] = 0
     });
 });
+
+
+
+
+function autoMapgenerator(startX,amount,gridSize){
+    //Rules
+    //world has max 2000 depth mountains and and min 500 depth flat land
+    //blocks should be connected and should not defy the laws of gravity(no fling blocks)
+    //no extreme changes (a tower of 2000 in an instant should not be possible)
+    let blocks = [];
+    for(let i=startX;i<startX+amount;i++){
+        collisonMap[i*gridSize] = {};
+        for(let k=64;k>25;k--){
+            let block = {};
+
+
+            collisonMap[i *gridSize][k*gridSize] = true;
+            block["x"] = i * gridSize;
+            block["y"] = k * gridSize;
+            block["type"] = "dirt";
+            block["health"] = 100;
+            blocks.push(block);
+        }
+
+    }
+    console.log(blocks);
+    console.log(collisonMap);
+    return blocks;
+}
+
+//64px 64px
+function checkCollision(player,sizex,sizey,gridSize){
+    let MAXX = player.x + sizex + (gridSize - ((player.x + sizex) % gridSize )) ;
+    let MINX = player.x - sizex - ((player.x - sizex) % gridSize ) ;
+    let MAXY = player.y + sizey + (gridSize - ((player.y + sizey) % gridSize )) ;
+    let MINY = player.y-sizey - ((player.y - sizey) % gridSize ) ;
+    console.log("Checking collision for = " + player.x+ "," + player.y);
+    console.log(MAXX+ "," + MINX);
+    console.log(MAXY+ "," + MINY);
+
+    for (let i = MINX; i <= MAXX ; i += gridSize) {
+        for (let j = MINY; j < MAXY; j+= gridSize) {
+            try {
+                if (collisonMap[i][j] === undefined) {
+                    console.log("no collision")
+                    return false;
+                } else {
+                    return true;
+
+                }
+            }catch (e) {
+                return false;
+            }
+        }
+    }
+
+}
+
 
 setInterval(function() {
     io.sockets.emit('state', players);
